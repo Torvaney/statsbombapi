f4e678952529e1ca481775fcab71ea6bcfa91d61
Torvaney <torvaney@protonmail.com>
2020-04-17 00:02:35 +0100

    Import everything from .json

--- statsbombapi/__init__.py
+++ statsbombapi/__init__.py
@@ -1,16 +1,4 @@
-from statsbombapi.json import (
-    Gender,
-    Competition,
-    Season,
-    MatchStatus,
-    Match,
-    Team,
-    parse_competitions,
-    parse_matches,
-    parse_lineups,
-    parse_events,
-    extract
-)
+from statsbombapi.json import *
 from statsbombapi.client import (
     StatsbombPublic,
     StatsbombAPI,
